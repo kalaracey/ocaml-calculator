8489f7edb52061a5660598568c5f702c00a88048
Kyle Laracey <kalaracey@gmail.com>
2024-04-21 21:14:57 -0400

    Big- and small-step operational semantics

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 https://cs3110.github.io/textbook/chapters/interp/parsing.html
+
+```bash
+dune build
+opam install --deps-only .
+```
+
+Then run `dune utop lib`:
+
+```ocaml
+utop # Interp.Main.interp_small "let x = 3110 in x + x";;
+- : Interp.Ast.expr = Interp.Ast.Int 6220
+```
--- lib/main.ml
+++ lib/main.ml
@@ -4,3 +4,104 @@ let parse (s : string) : expr =
   let lexbuf = Lexing.from_string s in
   let ast = Parser.prog Lexer.read lexbuf in
   ast
+
+(** The error message produced if a variable is unbound. *)
+  let unbound_var_err = "Unbound variable"
+
+(** The error message produced if binary operators and their
+    operands do not have the correct types. *)
+let bop_err = "Operator and operand type mismatch"
+
+(** The error message produced if the [then] and [else] branches
+    of an [if] do not have the same type. *)
+let if_branch_err = "Branches of if must have same type"
+
+(** The error message produced if the guard
+    of an [if] does not have type [bool]. *)
+let if_guard_err = "Guard of if must have type bool"
+
+(** [is_value e] is whether [e] is a value. *)
+let is_value : expr -> bool = function
+  | Int _ | Bool _ -> true
+  | Var _ | Let _ | Binop _ | If _ -> false
+
+(** [subst e v x] is [e] with [v] substituted for [x], that
+    is, [e{v/x}]. *)
+    let rec subst e v x = match e with
+    | Var y -> if x = y then v else e
+    | Bool _ -> e
+    | Int _ -> e
+    | Binop (bop, e1, e2) -> Binop (bop, subst e1 v x, subst e2 v x)
+    | Let (y, e1, e2) ->
+      let e1' = subst e1 v x in
+      if x = y
+      then Let (y, e1', e2)
+      else Let (y, e1', subst e2 v x)
+    | If (e1, e2, e3) ->
+      If (subst e1 v x, subst e2 v x, subst e3 v x)
+
+(** [step] is the [-->] relation, that is, a single step of
+    evaluation. *)
+let rec step : expr -> expr = function
+  | Int _ | Bool _ -> failwith "Does not step"
+  | Var _ -> failwith unbound_var_err
+  | Binop (bop, e1, e2) when is_value e1 && is_value e2 ->
+    step_bop bop e1 e2
+  | Binop (bop, e1, e2) when is_value e1 ->
+    Binop (bop, e1, step e2)
+  | Binop (bop, e1, e2) -> Binop (bop, step e1, e2)
+  | Let (x, e1, e2) when is_value e1 -> subst e2 e1 x
+  | Let (x, e1, e2) -> Let (x, step e1, e2)
+  | If (Bool true, e2, _) -> e2
+  | If (Bool false, _, e3) -> e3
+  | If (Int _, _, _) -> failwith if_guard_err
+  | If (e1, e2, e3) -> If (step e1, e2, e3)
+
+(** [step_bop bop v1 v2] implements the primitive operation
+    [v1 bop v2].  Requires: [v1] and [v2] are both values. *)
+and step_bop bop e1 e2 = match bop, e1, e2 with
+  | Add, Int a, Int b -> Int (a + b)
+  | Mult, Int a, Int b -> Int (a * b)
+  | Leq, Int a, Int b -> Bool (a <= b)
+  | _ -> failwith bop_err
+
+(** [eval_small e] is the [e -->* v] relation.  That is,
+    keep applying [step] until a value is produced.  *)
+let rec eval_small (e : expr) : expr =
+  if is_value e then e
+  else e |> step |> eval_small
+
+(** [interp_small s] interprets [s] by parsing, type-checking,
+    and evaluating it with the small-step model. *)
+let interp_small (s : string) : expr =
+  let e = parse s in
+  (* typecheck e; *)
+  eval_small e
+
+(** [eval_big e] is the [e ==> v] relation. *)
+let rec eval_big (e : expr) : expr = match e with
+  | Int _ | Bool _ -> e
+  | Var _ -> failwith unbound_var_err
+  | Binop (bop, e1, e2) -> eval_bop bop e1 e2
+  | Let (x, e1, e2) -> subst e2 (eval_big e1) x |> eval_big
+  | If (e1, e2, e3) -> eval_if e1 e2 e3
+
+(** [eval_bop bop e1 e2] is the [e] such that [e1 bop e2 ==> e]. *)
+and eval_bop bop e1 e2 = match bop, eval_big e1, eval_big e2 with
+  | Add, Int a, Int b -> Int (a + b)
+  | Mult, Int a, Int b -> Int (a * b)
+  | Leq, Int a, Int b -> Bool (a <= b)
+  | _ -> failwith bop_err
+
+(** [eval_if e1 e2 e3] is the [e] such that [if e1 then e2 else e3 ==> e]. *)
+and eval_if e1 e2 e3 = match eval_big e1 with
+  | Bool true -> eval_big e2
+  | Bool false -> eval_big e3
+  | _ -> failwith if_guard_err
+
+(** [interp_big s] interprets [s] by parsing, type-checking,
+  and evaluating it with the big-step model. *)
+let interp_big (s : string) : expr =
+  let e = parse s in
+  (* typecheck e; *)
+  eval_big e
